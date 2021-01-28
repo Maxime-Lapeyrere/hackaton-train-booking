f35b06ca3af314cd0b084efab433a2ace928e4e8
Judith <plvjudith@live.fr>
2021-01-28 15:27:23 +0100

    /search route ok

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
 var users = require('../models/users');
+var journeys = require('../models/journeydb');
 var SHA256 = require("crypto-js/sha256");
 var encBase64 = require('crypto-js/enc-base64');
 
+var capitalize = (word) => {
+  var letters = word.slice('');
+  var capitalizedWord = letters[0].toUpperCase();
+  for (var i = 1; i < letters.length; i++) {
+    capitalizedWord += letters[i].toLowerCase();
+  }
+  return capitalizedWord;
+};
 
 var createUser = async (data) => {
   var newUser = new users ({
-    lastName: data.lastName.toLowerCase(),
-    firstName: data.firstName.toLowerCase(),
+    lastName: capitalize(data.lastName),
+    firstName: capitalize(data.firstName),
     email: data.email.toLowerCase(),
     pwd: SHA256(data.pwd).toString(encBase64),
     journeys: [ ]
   });
-  console.log("newUser : ");//DEBUG
-  console.log(newUser);//DEBUG
   var userSaved = await newUser.save();
-  console.log("newUser_DB : ");//DEBUG
-  console.log(await users.find());//DEBUG
-
   return userSaved;
 };
 
@@ -27,7 +30,7 @@ var createUser = async (data) => {
 router.get('/', function(req, res, next) {
   if (!req.session.email)
     res.redirect('/login');
-  res.render('journey', {routename: ''});
+  res.render('journey', { routename: '' });
 });
 
 // LOGIN PAGE
@@ -37,9 +40,7 @@ router.get('/login', async function(req, res, next) {
 
 // SIGN-IN
 router.post('/sign-in', async function(req, res, next) {
-  console.log(req.body);//DEBUG
   var shapwd = SHA256(req.body.pwd).toString(encBase64);
-  console.log(shapwd);
   var user = await users.findOne({ email : req.body.email.toLowerCase(), pwd : shapwd });
   if (user) {
     req.session.email = user.email;
@@ -53,7 +54,6 @@ router.post('/sign-in', async function(req, res, next) {
 
 // SIGN-UP
 router.post('/sign-up', async function(req, res, next) {
-  console.log(req.body);
   var user = await users.findOne({ email : req.body.email.toLowerCase() });
   if (user) {
     res.render('login', { error: "L'utilisateur existe déjà, utilisez le formulaire sign-in", routename: 'login' });
@@ -69,6 +69,15 @@ router.post('/sign-up', async function(req, res, next) {
 router.get('/logout', function(req, res, next) {
   req.session.destroy();
   res.redirect('/');
-})
+});
+
+router.post('/search', async function(req, res, next) {
+  console.log(req.body);
+  var departure = capitalize(req.body.departure);
+  var arrival = capitalize(req.body.arrival);
+  var trips = await journeys.find({ departure : departure, arrival: arrival, date: req.body.date});
+  console.log(trips);
+  res.render('/search-results', { journeys: trips });
+});
 
 module.exports = router;
