d5e5318b51d74a2a9e34a89c382856413a9ab256
Judith <plvjudith@live.fr>
2021-01-28 14:43:22 +0100

    fixed sign-in and sign-up routes

--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,74 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-var users = require('../models/users')
+var users = require('../models/users');
+var SHA256 = require("crypto-js/sha256");
+var encBase64 = require('crypto-js/enc-base64');
 
 
 var createUser = async (data) => {
   var newUser = new users ({
-    lastName: data.lastNametoLowerCase(),
+    lastName: data.lastName.toLowerCase(),
     firstName: data.firstName.toLowerCase(),
     email: data.email.toLowerCase(),
-    pwd: data.pwd,
+    pwd: SHA256(data.pwd).toString(encBase64),
     journeys: [ ]
   });
   console.log("newUser : ");//DEBUG
   console.log(newUser);//DEBUG
   var userSaved = await newUser.save();
   console.log("newUser_DB : ");//DEBUG
-  console.log(await userModel.find());//DEBUG
+  console.log(await users.find());//DEBUG
 
   return userSaved;
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (!req.session.user)
-    res.redirect('/login', { error: ""});
+  if (!req.session.email)
+    res.redirect('/login');
   res.render('index', { title: 'Ticketac' });
 });
 
 // LOGIN PAGE
 router.get('/login', async function(req, res, next) {
-  res.render('/login', { error: ""});
+  res.render('login', { error: ""});
 });
 
 // SIGN-IN
 router.post('/sign-in', async function(req, res, next) {
   console.log(req.body);//DEBUG
-  var user = await userModel.findOne({ email : req.body.email.toLowerCase(), pwd : req.body.pwd});
+  var shapwd = SHA256(req.body.pwd).toString(encBase64);
+  console.log(shapwd);
+  var user = await users.findOne({ email : req.body.email.toLowerCase(), pwd : shapwd });
   if (user) {
-    req.session.user.name = user.name;
-    req.session.user.id = user._id;
-    res.redirect('/', { user: user});
+    req.session.email = user.email;
+    req.session.id = user._id;
+    res.redirect('/');
   }
   else {
-    res.render('/login', { error: "Utilisateur inconnu"});
+    res.render('login', { error: "Utilisateur inconnu"});
   }
 });
 
 // SIGN-UP
 router.post('/sign-up', async function(req, res, next) {
-  var user = await userModel.findOne({ email : req.body.email.toLowerCase()});
+  console.log(req.body);
+  var user = await users.findOne({ email : req.body.email.toLowerCase() });
   if (user) {
-    res.redirect('/login', { error: "L'utilisateur existe déjà, utilisez le formulaire sign-in"});
+    res.render('login', { error: "L'utilisateur existe déjà, utilisez le formulaire sign-in" });
   }
   else {
-    user = createUser(req.body);
-    req.session.user.name = user.name;
-    req.session.user.id = user._id;
+    user = await createUser(req.body);
+    req.session.email = user.email;
+    req.session.id = user._id;
     res.render('/');
   }
 });
+
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.redirect('/');
+})
 
 module.exports = router;
