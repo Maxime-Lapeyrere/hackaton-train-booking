a6642f3b9c937e1e69279f35fed65bd380866824
Judith <plvjudith@live.fr>
2021-01-29 10:50:19 +0100

    fixing conflict

--- models/users.js
+++ models/users.js
@@ -6,8 +6,9 @@ var UserSchema = mongoose.Schema({
   firstName: String,
   email: String,
   pwd: String,
-  journeys: [ 
-    { type: mongoose.Schema.Types.ObjectId, ref: 'journeys' } ]
+  journeys: [ { 
+    type: mongoose.Schema.Types.ObjectId, ref: 'journeys' , 
+    quantity: Number } ]
 });
 
 var userModel = mongoose.model('users', UserSchema);
